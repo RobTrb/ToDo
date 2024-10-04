d3ff27544d38b6cddf68f05ca9b1b2e47c33932e
RobTrb <robert.trb@gmail.com>
2024-10-04 14:36:30 +0200

    forgot to add questions as comments

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
 /*
+Uppgiften/frågor:
+Todo-appen ska ha styling som motsvarar det som visas i den bifogade videon, avseende färger, mått, typografi, etc.
+     Innan själva huvudfrågan... Vilken font är det du använder i exemplet så man kan ändra till den fonten?
+     
+     Fråga: Hur nära i utseendet i videon måste man vara? Frågar mest för jag kan gärna i bästa möjliga utsträckning visa att jag kan och vill lära mig utefter hur ni instruerar.
+     Men med det sagt så har jag redan stylat min lista utefter hur jag själv ville ha den :) och jag frågar mest eftersom listan rent designmässigt är hur jag vill ha det och desto
+     mer jag ändrar för att få den att likna exemplet desto mer kommer jag behöva ta bort sen när jag fått betyget. Undrar om det finns någon lösning här där jag kan demonstrera att
+     jag kan det ni vill utan att ändra för mycket?
+
+När man kommer in på sidan ska hela sidan animeras enligt videofilen: flyga ner uppifrån och fade in
+     Denna delen är klar
+
+När man lägger till nya punkter i listan genom att skriva in dem och klicka på OK ska de animeras in enligt videofilen: fade-in och glidning uppåt
+
+    Fråga: Går detta att lösa med hur jag i grunden har programmerat min toDo lista? I och med att jag för att skapa li elementen har gjort en form av refresh funktion som
+    tar hela min array och skapar li element av objekten i min array så leder detta till att "fade-in och glidning uppåt" animationen appliceras på varje item i min UL varje
+    gång refresh funktionen kallas. Vill gärna bli pekad i rätt riktning på vad jag ska googla efter för att hitta en lösning?
+
+När man klickar på en punkt för att markera den som klar ska den animeras enligt videofilen: Fade till grå
+
+     Denna delen är klar
+
+Om man försöker lägga till en tom punkt ska felmeddelandet animeras enligt videofilen: Blinka rött
+
+     Denna delen är klar
+*/
+
+/*
 Notes to future me:
 
 Practice more with arrays and how it works with index push.
